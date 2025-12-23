a25af97b5e87fc68bf7b17f39dfe748824db8a33
GabrielAvelarbr <99gabrielavelar@gmail.com>
2025-12-23 10:35:27 -0300

    feat: add validation annotations to user dto

--- todo/src/main/java/io/github/gabrielvavelar/todo/user/controller/UserController.java
+++ todo/src/main/java/io/github/gabrielvavelar/todo/user/controller/UserController.java
@@ -4,6 +4,7 @@ import io.github.gabrielvavelar.todo.user.dto.UserRequestDto;
 import io.github.gabrielvavelar.todo.user.dto.UserResponseDto;
 import io.github.gabrielvavelar.todo.user.model.User;
 import io.github.gabrielvavelar.todo.user.service.UserService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,7 +20,7 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("/register")
-    public ResponseEntity<UserResponseDto> createUser(@RequestBody UserRequestDto dto) {
+    public ResponseEntity<UserResponseDto> createUser(@RequestBody @Valid UserRequestDto dto) {
         return ResponseEntity
                 .status(HttpStatus.CREATED)
                 .body(userService.save(dto));
--- todo/src/main/java/io/github/gabrielvavelar/todo/user/dto/UserRequestDto.java
+++ todo/src/main/java/io/github/gabrielvavelar/todo/user/dto/UserRequestDto.java
@@ -1,7 +1,11 @@
 package io.github.gabrielvavelar.todo.user.dto;
 
+import jakarta.validation.constraints.NotBlank;
+
 public record UserRequestDto(
-   String username,
-   String password
+        @NotBlank(message = "Username must not be blank")
+        String username,
+        @NotBlank(message = "Password must not be blank")
+        String password
 )
 {}
